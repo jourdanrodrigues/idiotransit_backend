71419acadbc61940325fb4329e36839bd0d3a3e6
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-09-04 21:10:21 -0300

    :sparkles: Add models and its migrations.

---
+++ idiotransit/core/models.py
@@ -0,0 +1,53 @@
+from uuid import uuid4
+
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.db import models, transaction
+from django.db.models import SET_NULL
+from django.utils.translation import ugettext as _
+
+IdField = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+tweet_length = 140
+
+
+class Reply(models.Model):
+    id = IdField
+    # Business Rule: Replies must be deleted on user delete method, so this models needs to be before "User".
+    # Since "Occurrence" model doesn't exist yet, it can be referenced by a string.
+    occurrence = models.ForeignKey('Occurrence', related_name='replies')
+    message = models.CharField(max_length=tweet_length)
+
+
+class User(AbstractBaseUser):
+    id = IdField
+    name = models.CharField(_('Name'), max_length=80)
+    email = models.EmailField('Email', unique=True)
+
+    USERNAME_FIELD = 'email'
+
+    @transaction.atomic
+    def delete(self, **kwargs):
+        Reply.objects.filter(occurrence__vehicle__owner=self).distinct().delete()
+        return super(User, self).delete(**kwargs)
+
+    def get_full_name(self):  # "Interface" method
+        return self.name
+
+    def get_short_name(self):  # "Interface" method
+        return self.name
+
+
+class Vehicle(models.Model):
+    id = IdField
+    owner = models.ForeignKey(User, null=True, on_delete=SET_NULL, related_name='vehicles')
+    license_plate = models.CharField(_('License Plate'), max_length=8)
+    color = models.CharField(_('Color'), max_length=20, null=True)
+    model = models.CharField(_('Model'), max_length=40, null=True)
+    brand = models.CharField(_('Brand'), max_length=40, null=True)
+
+
+class Occurrence(models.Model):
+    id = IdField
+    user = models.ForeignKey(User, null=True, on_delete=SET_NULL, related_name='occurrences')
+    vehicle = models.ForeignKey(Vehicle, related_name='occurrences')
+    description = models.CharField(max_length=tweet_length)
+    date_time = models.DateTimeField(auto_now_add=True, editable=False)
--- idiotransit/settings.py
+++ idiotransit/settings.py
@@ -76,4 +76,6 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = False
 
+AUTH_USER_MODEL = 'core.User'
+
 STATIC_URL = '/static/'
