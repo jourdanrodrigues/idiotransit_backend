caea0bf21bc52cfbb4a13932d85e2dd092d4189a
Jourdan Rodrigues <thiagojourdan@gmail.com>
2017-09-10 21:06:41 -0300

    :umbrella: Add tests for vehicle creation.

--- idiotransit/core/tests.py
+++ idiotransit/core/tests.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from django.test import TestCase
 
 from idiotransit.core.models import Vehicle, Occurrence, User
@@ -11,6 +12,17 @@ class VehicleTestCase(TestCase):
 
         with self.assertRaises(NotImplementedError):
             vehicle.delete()
+
+    def test_vehicle_creation(self):
+        license_plate = 'ASD-1234'
+        common_args = {'license_plate': license_plate}
+
+        Vehicle.objects.create(**common_args)
+        self.assertTrue(Vehicle.objects.filter(owner__isnull=True, **common_args).exists())
+
+        with self.assertRaises(IntegrityError):
+            # Since license plate must be unique, a second creation of the same license plate should not be allowed
+            Vehicle.objects.create(**common_args)
 
 
 class OccurrenceTestCase(TestCase):
